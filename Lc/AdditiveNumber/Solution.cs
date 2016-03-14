7c1480ef817e55e4459a9ac0e4dd6b47d2119b21
stephenwan <stephenwyy@gmail.com>
2016-03-14 19:37:13 +0800

    Active Number

---
+++ Lc/AdditiveNumber/Solution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Int16;
+using static System.Int64;
+
+namespace Lc.AdditiveNumber
+{
+    public class Solution
+    {
+        public bool IsAdditiveNumber(string num)
+        {            
+            if (num.Length == 0) return false;
+
+            for (var i = 1; i < num.Length/2 + 1; i ++)
+            {                                
+                for (var j = 1; j <= i; j++)
+                {
+                    var pend = num.Length;
+                    var pmid = pend - i;
+                    var pstart = pmid - j;
+
+                    var toContinue = true;
+
+                    while (toContinue)
+                    {
+                        var next = getNextPoint(num, pstart, pmid, pend);
+
+                        if (next == 0) return true;
+                        if (next < 0) toContinue = false;
+                        
+                        pend = pmid;
+                        pmid = pstart;
+                        pstart = next;
+                    }                  
+                }
+            }
+            return false;
+        }
+
+        private int getNextPoint(string s, int pstart, int pmid, int pend)
+        {
+            if (pstart == 0) return -1;
+            if (s[pmid] == '0' && pmid < pend - 1) return -1;
+            if (s[pstart] == '0' && pstart < pmid - 1) return -1;
+             
+            var sum = Int64.Parse(s.Substring(pmid, pend - pmid));
+            var rpart = Int64.Parse(s.Substring(pstart, pmid - pstart));
+         
+            var lpart = sum - rpart;
+            if (lpart < 0) return -1;
+            if (lpart == 0) return (s[pstart - 1] == '0') ? pstart - 1 : -1;
+
+
+            var strLpart = lpart.ToString();
+            if (strLpart.Length > pstart) return -1;
+
+            return s.Substring(pstart - strLpart.Length, strLpart.Length) == strLpart
+                ? pstart - strLpart.Length
+                : -1;
+        }
+       
+    }
+}
---
+++ LcTest/AdditiveNumber/SolutionTest.cs
@@ -0,0 +1,37 @@
+using Lc.AdditiveNumber;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace LcTest.AdditiveNumber
+{
+	[TestFixture]
+	public class SolutionTest
+	{
+		private readonly Solution _solution = new Solution();		
+
+		[Test]
+		public void Test1()
+		{
+			_solution.IsAdditiveNumber("112358").Should().Be.True();
+		}
+
+        [Test]
+        public void Test2()
+        {
+            _solution.IsAdditiveNumber("1203").Should().Be.False();
+        }
+
+        [Test]
+        public void Test3()
+        {
+            _solution.IsAdditiveNumber("0123").Should().Be.False();
+        }
+
+	    [Test]
+	    public void Test4()
+	    {
+	        _solution.IsAdditiveNumber("11235813213455890144").Should().Be.True();
+	    }
+
+    }
+}
